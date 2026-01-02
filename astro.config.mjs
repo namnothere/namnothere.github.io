44010dabab7c1bbb3d4611ce1389aad90c5b29b6
namnothere <canhnamtran216@gmail.com>
2026-01-03 01:32:44 +0700

    fix: image optimize issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,12 @@ import image from '@astrojs/image';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://namnothere.github.io',
-  base: '/namnothere.github.io',
+  // base: '/namnothere.github.io',
   integrations: [image()],
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  image: {
+      domains: ["astro.build", "images.unsplash.com"],
+    }
 });
