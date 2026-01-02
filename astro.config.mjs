826136604cca54d4d5762e22f9639379fcfdf726
namnothere <canhnamtran216@gmail.com>
2026-01-03 00:37:50 +0700

    feat: update config + image optimize

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://namnothere.github.io',
+  base: '/namnothere.github.io',
+  integrations: [image()],
   vite: {
     plugins: [tailwindcss()]
   }
